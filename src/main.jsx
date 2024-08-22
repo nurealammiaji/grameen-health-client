6d303c1ed6b507a3345c53f81f2809a2056682b7
nurealammiaji <nurealam.miaji@gmail.com>
2024-08-22 22:19:21 +0600

    router provider added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider } from "react-router-dom";
+import AllRoutes from './routes/AllRoutes.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={AllRoutes} >
+      <App />
+    </RouterProvider>
   </StrictMode>,
 )
