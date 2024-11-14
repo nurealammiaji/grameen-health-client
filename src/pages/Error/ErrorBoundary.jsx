bbf9b8f45a74c051e7bb31afa6a6a87b50996625
nurealammiaji <nurealam.miaji@gmail.com>
2024-11-14 22:03:28 +0600

    go to home button added to error page

--- src/pages/Error/ErrorBoundary.jsx
+++ src/pages/Error/ErrorBoundary.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { isRouteErrorResponse, useRouteError } from 'react-router-dom'
+import { RiArrowLeftLine } from 'react-icons/ri';
+import { isRouteErrorResponse, Link, useRouteError } from 'react-router-dom'
 
 const ErrorBoundary = () => {
 
@@ -9,50 +10,71 @@ const ErrorBoundary = () => {
   if (isRouteErrorResponse(error)) {
     if (error.status === 404) {
       return (
-        <div className="mt-10 mx-auto text-center text-2xl">
+        <div className="mx-auto mt-10 text-2xl text-center">
           <div className="font-semibold">{error.status}</div>
           <div className="my-5 font-bold">{error.statusText}</div>
           <div className="italic">{error.data}</div>
-          <div className='mt-10 text-red-500'>Page not found</div>
+          <div className="mt-10 text-red-500">Page not found</div>
+          <div className="mt-16 text-center">
+            <Link to={"/"} className="text-lg font-bold btn btn-success btn-outline"><RiArrowLeftLine className="mr-2 text-2xl" />Go to Home</Link>
+          </div>
         </div>
       );
     }
 
     if (error.status === 401) {
       return (
-        <div className="mt-10 mx-auto text-center text-2xl">
+        <div className="mx-auto mt-10 text-2xl text-center">
           <div className="font-semibold">{error.status}</div>
           <div className="my-5 font-bold">{error.statusText}</div>
           <div className="italic">{error.data}</div>
           <div className="mt-10 text-red-500">You aren't authorized to see this</div>
+          <div className="mt-16 text-center">
+            <Link to={"/"} className="text-lg font-bold btn btn-success btn-outline"><RiArrowLeftLine className="mr-2 text-2xl" />Go to Home</Link>
+          </div>
         </div>
       );
     }
 
     if (error.status === 503) {
       return (
-        <div className="mt-10 mx-auto text-center text-2xl">
+        <div className="mx-auto mt-10 text-2xl text-center">
           <div className="font-semibold">{error.status}</div>
           <div className="my-5 font-bold">{error.statusText}</div>
           <div className="italic">{error.data}</div>
           <div className='mt-10 text-red-500'>Looks like our API is down</div>
+          <div className="mt-16 text-center">
+            <Link to={"/"} className="text-lg font-bold btn btn-success btn-outline"><RiArrowLeftLine className="mr-2 text-2xl" />Go to Home</Link>
+          </div>
         </div>
       );
     }
 
     if (error.status === 418) {
       return (
-        <div className="mt-10 mx-auto text-center text-2xl">
+        <div className="mx-auto mt-10 text-2xl text-center">
           <div className="font-semibold">{error.status}</div>
           <div className="my-5 font-bold">{error.statusText}</div>
           <div className="italic">{error.data}</div>
           <div className='mt-10 text-red-500'>🫖</div>
+          <div className="mt-16 text-center">
+            <Link to={"/"} className="text-lg font-bold btn btn-success btn-outline"><RiArrowLeftLine className="mr-2 text-2xl" />Go to Home</Link>
+          </div>
         </div>
       );
     }
   }
 
-  return <div>Something went wrong</div>;
+  return <>
+    <div>
+      <div className="text-center">
+        <h1 className="text-3xl font-bold text-error">Something went wrong</h1>
+      </div>
+      <div className="mt-16 text-center">
+        <Link to={"/"} className="text-lg font-bold btn btn-success btn-outline"><RiArrowLeftLine className="mr-2 text-2xl" />Go to Home</Link>
+      </div>
+    </div>
+  </>;
 }
 
 export default ErrorBoundary
