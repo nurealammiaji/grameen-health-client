750a94ec8a85760937ed7248e0389d57ad940f53
nurealammiaji <nurealam.miaji@gmail.com>
2024-12-30 23:38:15 +0600

    merchant profile added

--- src/pages/Dashboard/Merchant/MerchantProfile/MerchantProfile.jsx
+++ src/pages/Dashboard/Merchant/MerchantProfile/MerchantProfile.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import HelmetAsync from '../../../../components/HelmetAsync/HelmetAsync';
+import useUser from '../../../../hooks/useUser';
+import ProfileDetails from '../../../../components/ProfileDetails/ProfileDetails';
 
 const MerchantProfile = () => {
+
+    const { isUserLoading, isUserError, userData, refetchUser, userError } = useUser();
+
     return (
         <div>
             <HelmetAsync title={"Profile"} />
@@ -9,6 +14,10 @@ const MerchantProfile = () => {
                 <div className="mx-auto sm:w-6/12 divider divider-success"><h3 className="px-4 pt-1 pb-2 text-2xl font-bold text-white bg-success">Profile</h3></div>
             </div>
             <br /><br />
+            {
+                userData &&
+                <ProfileDetails profileData={userData} />
+            }
         </div>
     );
 };
